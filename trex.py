8be1feef74855cd0fb2ed94ae9a35037da385a84
nirmalthomas2609 <nirmalthomas26@gmail.com>
2017-02-18 18:14:28 +0000

    Land moving + trex running (cactus movement not added) Check testtrex.py file

--- trex.py
+++ trex.py
@@ -33,8 +33,9 @@ clock=pygame.time.Clock()
 scr.fill((255,255,255))
 score=0
 speed = 5
+trw=runl.get_width()
 def preprocess():
-	scr.blit(land1,(0,200))
+#	scr.blit(land1,(0,200))
 	scr.blit(runl,(55,167))
 	i=random.randint(1,4)
 	t=[]
@@ -47,29 +48,109 @@ def preprocess():
 		else:
 			scr.blit(cactbig,(s,200-(cactbig.get_rect().size[1])+15))
 			s+=cactbig.get_rect().size[0]-2
-def gameloop():
-	l=0
-	while (1):
-		run(l)
-		if (l==0):
-			l=1
+class main ():
+	def __init__(self):
+		self.l=0
+		self.h=0
+		self.landx=0
+		self.landy=750
+		self.xshift=land1.get_width()-750-4
+		self.k=0
+	def run(self):
+		if(self.l==0):
+			scr.blit(runl,(55,167))
+			self.l=1
 		else:
-			l=0
-		pygame.display.update()
+			scr.blit(runr,(55,167))
+			self.l=0
+		'''
+		position=land1.get_rect().move(self.k,200)
+		scr.blit(land1,position)
+		self.k+=land1.get_width()
+		print (self.k)
+		'''
+		'''
+				#printing land
+		scr.blit(land1, (-self.landx, 200))
+
+		#checking if land1 has shifted enough to introduce land2
+		if(self.landx>=self.xshift):
+			scr.blit(land1, (-self.land2x, 200))
+			self.land2x+=50
+
+		#Replacing land by land2 after checking if land has completely gone out of screen
+		if(self.landx>land1.get_width()):
+			self.landx=self.land2x
+			self.land2x=-750
+		#New land position
+		self.landx+=50
+		'''
+
+		'''
+		if (self.h==0):
+			scr.blit(land1,(self.landx,200))
+			scr.blit(land2,(self.landy,200))
+			self.h=1
+			self.landx-=50
+			self.landy-=50
+		else:
+			position1=land1.get_rect().move(self.landx,200)
+			position2=land2.get_rect().move(self.landy,200)
+			self.landx-=50
+			self.landy-=50
+			scr.blit(land1,position1)
+			scr.blit(land2,position2)
+			if(self.landx<=(0-land1.get_width())):
+				self.landx=self.landy+land2.get_width()
+			elif(self.landy<=(0-land2.get_width())):
+				self.landy=self.landx+land1.get_width()
+		'''
+		'''
+		if (self.landx>=(750-land1.get_width())):
+			scr.blit(land1,(self.landx,200))
+			self.landx-=50
+		if(self.k==0):
+			scr.blit(runl,(100,100))
+			self.k=1
+		else:
+			scr.blit(runl,(550,100))
+			self.k=0
+		if(self.landx>=(750-land1.get_width())):
+#			pygame.draw.rect(scr,(255,255,255),(self.landx+50,200,land1.get_width(),land1.get_height()))
+
+			scr.blit(land1,(self.landx,200))
+			self.landx-=50
+		elif(self.landx<=-(land1.get_width())):
+			scr.blit(land1,(0,200))
+			self.landx=0
+		elif(self.landx<=(750-land1.get_width())):
+			pygame.draw.rect(scr,(255,255,255),(self.landx+50,200,land1.get_width(),land1.get_height()))
+			pygame.draw.rect(scr,(255,255,255),(self.landx+50+land1.get_width(),20,land1.get_width(),land1.get_height()))
+			scr.blit(land1,(self.landx,200))
+			scr.blit(land1,(self.landx+land1.get_width(),200))
+			self.landx-=50
+		if (self.landx<=-(750-land1.get_rect().size[0])):
+			self.landx=0
+		
+		scr.blit(land1,(self.landx,200))
+		scr.blit(land2,(self.landy,200))
+		self.landx-=50
+		self.landy-=50
+		if self.landx==-(land1.get_rect().size[0]):
+			self.landx=self.landy+land2.get_rect().size[0]
+		elif self.landy==-(land2.get_rect().size[0]):
+			self.landy=self.landx+land1.get_rect().size[0]
+		'''
 		clock.tick(10)
+		pygame.display.flip()
+preprocess()
+def game():
+	test=main()
+	while (1):
+		test.run()
 		for event in pygame.event.get():
 			if event.type==pygame.QUIT:
 				pygame.quit()
 				quit()
-def run(l):
-	if(l==0):
-		scr.blit(runl,(55,167))
-	else:
-		scr.blit(runr,(55,167))
-print (runl.get_rect().size[0])
-print (land1.get_rect().size[0])
-print (land2.get_rect().size[0])
-preprocess()
-gameloop()
-
+game()
 
